0d9f245b9e7377783f7fbb1a381175d980b170c7
SolisSaturnus <77696391+SolisSaturnus@users.noreply.github.com>
2021-04-15 19:06:04 -0500

    NavMesh Project

---
+++ Assets/EnemyControllerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnemyControllerScript : MonoBehaviour
+{
+
+    public NavMeshAgent enemyAgent;
+
+    public GameObject player;
+
+    void Start()
+    {
+        enemyAgent.updateRotation = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    void Update()
+    {
+        enemyAgent.SetDestination(player.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            PlayerController.lives--;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+	public ThirdPersonCharacter character;
+	public TextMeshProUGUI livesText;
+	public TextMeshProUGUI countText;
+	public GameObject Collectable;
+
+	public static int lives = 3;
+	public Camera cam;
+	private Rigidbody rb;
+	private int count;
+
+	public NavMeshAgent agent;
+
+	void Start()
+	{
+		agent.updateRotation = false;
+
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+
+		SetCountText();
+		Collectable.SetActive(false);
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 10)
+		{
+			Collectable.SetActive(true);
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+
+			count = count += 10;
+
+			SetCountText();
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				// MOVE OUR AGENT
+				agent.SetDestination(hit.point);
+			}
+
+		}
+		if (agent.remainingDistance > agent.stoppingDistance)
+		{
+			character.Move(agent.desiredVelocity, false, false);
+
+		}
+		else
+		{
+			character.Move(Vector3.zero, false, false);
+		}
+
+		if (lives <= 0)
+		{
+			livesText.text = "Lives: 0";
+		}
+		else
+		{
+			livesText.text = "Lives: " + lives.ToString();
+		}
+	}
+	
+
+}
+
+
+
+
+
